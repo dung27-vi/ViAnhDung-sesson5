cd4bf4789d575a7fb80a1f5a41307f69ed9cffb5
dung27-vi <duvi57836@gmail.com>
2025-10-28 14:02:14 +0700

    BTVN57

---
+++ Untitled507.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+    printf("Nhap mot ky tu: ");
+    scanf("%c", &c);
+
+    if (c >= 'a' && c <= 'z') {
+        c = c - 32;
+        printf("Ky tu hoa tuong ung: %c\n", c);
+    } else if (c >= 'A' && c <= 'Z') {
+        c = c + 32;
+        printf("Ky tu thuong tuong ung: %c\n", c);
+    } else {
+        printf("Khong phai chu cai.\n");
+    }
+
+    return 0;
+}
